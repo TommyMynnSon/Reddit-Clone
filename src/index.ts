ae261d2f9de8a9f0e41461b6c106c1fd0cb5f2e4
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-01 05:51:18 -0700

    Prepare statements to insert into the Post table

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,12 @@ const main = async () => {
     type: 'postgresql',
     debug: !__prod__
   });
+
+  const post = orm.em.create(Post, {title: 'my first pst'});
+  await orm.em.persistAndFlush(post);
+
+  console.log("---------------sql 2---------------s")
+  await orm.em.nativeInsert(Post, { title: "my first post 2" });
 };
 
 main();
